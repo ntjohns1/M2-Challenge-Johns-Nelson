4b4d95ea2afc14fdf88157a0d6d7d27acf885a01
ntjohns1 <nelsontjohns@gmail.com>
2022-03-01 22:44:22 -0500

    initial file structure and tests

---
+++ src/main/java/com/company/MonthAndMathService/controllers/MonthController.java
@@ -0,0 +1,4 @@
+package com.company.MonthAndMathService.controllers;
+
+public class MonthController {
+}
---
+++ src/main/java/com/company/MonthAndMathService/models/MathSolution.java
@@ -0,0 +1,4 @@
+package com.company.MonthAndMathService.models;
+
+public class MathSolution {
+}
